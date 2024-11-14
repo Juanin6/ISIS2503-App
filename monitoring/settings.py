6408e762b84ac3e0987da03dc2efa2b4836ff9f2
MarianaOrtegaRam <m.ortegar@uniandes.edu.co>
2024-11-13 22:49:38 -0500

    crea el hash en la base de datos

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,15 +78,16 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME' :'monitoring_db',
-        'USER': 'monitoring_user',
-        'PASSWORD': 'isis2503',
-        'HOST': '10.128.0.2',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + 'db.sqlite3',
     }
+
 }
 
 
