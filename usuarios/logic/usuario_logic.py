34b99e87c06d41bf7b0391887dc68b2ae741dbd7
Juanin6 <jn.suarez@uniandes.edu.co>
2024-12-04 18:02:50 +0000

    Mandar Solicitud Usuarios

--- usuarios/logic/usuario_logic.py
+++ usuarios/logic/usuario_logic.py
@@ -8,3 +8,7 @@ def create_usuario(form):
     measurement = form.save()
     measurement.save()
     return ()
+def check_usuario_exists(user_id):
+    # Comprobar si existe un usuario con el id proporcionado
+    exists = Usuario.objects.filter(id=user_id).exists()
+    return exists
