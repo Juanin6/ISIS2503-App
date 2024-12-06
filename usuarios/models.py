199dc91b7ee9978257ad3d4a87f8c6b4abe67544
CamiloD-96 <ca.daza10@uniandes.edu.co>
2024-12-05 20:59:37 -0500

    ddd

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from cursos.models import Curso
+#from cursos.models import Curso
 class Usuario(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(null=True,max_length=50)
@@ -7,7 +7,7 @@ class Usuario(models.Model):
     correo = models.CharField(max_length=50,default='correo@gmail.com')
     contrasena = models.CharField(max_length=50,default='1234')
     tipoUsuario = models.CharField(max_length=50,default='estudiante')
-    curso = models.ForeignKey(Curso, on_delete=models.CASCADE,null=True)
+#    curso = models.ForeignKey(Curso, on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return '{} ({})'.format(self.nombre, self.edad)
